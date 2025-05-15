dbf734a994a5e0f7a1951d65cd8f95d5da6c5d0e
kounolk <00g9meku@gmail.com>
2025-05-15 16:09:41 +0900

    profileのレスポンスでプロパティが含まれていない場合の修正

--- src/utils/types.utils.ts
+++ src/utils/types.utils.ts
@@ -14,13 +14,29 @@ export function parseFitbitScope(value: unknown): FitbitScope {
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function exists(json: any, key: string) {
   const value = json[key];
+
   return value !== null && value !== undefined;
 }
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function get<T>(obj: any, key: string): T {
+  // undefinedでも許可するキーを判定する
+  if (permissionKey(key)) {
+    return '' as T;
+  }
+
   if (!exists(obj, key)) {
     throw new Error(`Key not found: ${key} in ${JSON.stringify(obj)}`);
   }
   return obj[key] as T;
+}
+
+/**
+ * undefinedでも許可するキーを判定
+ * @param key
+ */
+function permissionKey(key: string) {
+  if (key === 'firstName' || key === 'lastName') {
+    return true;
+  }
 }
